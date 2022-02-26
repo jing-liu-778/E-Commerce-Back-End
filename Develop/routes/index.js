85a11701f808db009996d6f143a07c3b02fe7037
Jing Liu <jingliu778@gmail.com>
2022-02-25 21:28:55 -0500

    set route

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
+const router = require("express").Router();
+const apiRoutes = require("./api");
+// set api route
+router.use("/api", apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route!</h1>");
 });
 
 module.exports = router;
