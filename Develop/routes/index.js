d74ecfde06ca3497aef65971b4f570c28f21c565
Jing Liu <jingliu778@gmail.com>
2022-02-25 21:31:40 -0500

    update

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 // set api route
 router.use("/api", apiRoutes);
-
+// set wrong route
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>");
 });
