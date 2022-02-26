01250cf2eb088e2446d90e1259d1b7f2bfbc1116
Jing Liu <jingliu778@gmail.com>
2022-02-25 21:25:16 -0500

    tag update

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,9 +1,9 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
-
+// set up tag datatype
 Tag.init(
   {
     // define columns
@@ -11,19 +11,18 @@ Tag.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
-    tag_name:{
+    tag_name: {
       type: DataTypes.STRING,
-      
-    }
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
